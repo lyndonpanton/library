dd846f52656ce580489d65db6e5f7adb8b51bf2e
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-31 19:22:15 +0100

    Feature: Add book class, can add books to library

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function Book(title, author, pages, isRead) {
+    this.id = self.crypto.randomUUID();
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    let newBook = new Book(title, author, pages, isRead);
+    library.push(newBook);
+}
+
+const library = [];
+
 document.addEventListener("DOMContentLoaded", function(e) {
     console.log("DOM content has loaded");
 });
